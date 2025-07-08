19106b1ab271eeef47e50a4ed0ddad3727b16cee
akshayshahasane <akshayshahasane2527@gmail.com>
2025-07-08 18:44:34 +0530

    usermodel

---
+++ Learning-Application/src/main/java/com/example/Learning/Application/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.Learning.Application.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String role; // STUDENT, INSTRUCTOR, ADMIN
+}
