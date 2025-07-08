de05db9713092b884aab556f7a56517edbb9911b
akshayshahasane <akshayshahasane2527@gmail.com>
2025-07-08 20:52:32 +0530

    userservice.8.52

--- Learning-Application/src/main/java/com/example/Learning/Application/service/UserService.java
+++ Learning-Application/src/main/java/com/example/Learning/Application/service/UserService.java
@@ -2,11 +2,15 @@ package com.example.Learning.Application.service;
 
 import com.example.Learning.Application.dto.UserDto;
 import com.example.Learning.Application.entity.User;
+import jakarta.validation.constraints.Email;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
 
     User createUser(UserDto userDto);
     List<User> getAllUsers();
+
+    Optional<User> findByEmail(@Email(message = "Invalid email format") String email);
 }
