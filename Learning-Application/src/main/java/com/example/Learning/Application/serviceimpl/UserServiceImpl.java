7e86c9d57816482316663adb849d4cb84d6b55eb
akshayshahasane <akshayshahasane2527@gmail.com>
2025-07-08 18:46:28 +0530

    userserviceimpl

---
+++ Learning-Application/src/main/java/com/example/Learning/Application/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.Learning.Application.serviceimpl;
+
+import com.example.Learning.Application.entity.User;
+import com.example.Learning.Application.repository.UserRepository;
+import com.example.Learning.Application.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
