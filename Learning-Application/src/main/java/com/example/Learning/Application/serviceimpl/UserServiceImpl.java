8ceea0e6b6c741e63180c32ff8c72274ed504296
akshayshahasane <akshayshahasane2527@gmail.com>
2025-07-08 20:53:09 +0530

    userserviceimpl.8.52

--- Learning-Application/src/main/java/com/example/Learning/Application/serviceimpl/UserServiceImpl.java
+++ Learning-Application/src/main/java/com/example/Learning/Application/serviceimpl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -40,4 +41,10 @@ public class UserServiceImpl implements UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
 }
