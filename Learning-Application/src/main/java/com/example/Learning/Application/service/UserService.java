bd38730d1b268ddd449968f0d8e8b7ac96b32dee
akshayshahasane <akshayshahasane2527@gmail.com>
2025-07-08 19:35:43 +0530

    userservice1

--- Learning-Application/src/main/java/com/example/Learning/Application/service/UserService.java
+++ Learning-Application/src/main/java/com/example/Learning/Application/service/UserService.java
@@ -1,11 +1,12 @@
 package com.example.Learning.Application.service;
 
+import com.example.Learning.Application.dto.UserDto;
 import com.example.Learning.Application.entity.User;
 
 import java.util.List;
 
 public interface UserService {
 
-    User createUser(User user);
+    User createUser(UserDto userDto);
     List<User> getAllUsers();
 }
